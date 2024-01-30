2277c37096165f232353b0ffff7d914dd110b147
Nguyen Duc Thanh <thanh.nguyen@beetech.com>
2024-01-30 23:08:16 +0700

    add palindrome

--- palindrome/code.js
+++ palindrome/code.js
@@ -6,21 +6,23 @@ var isPalindrome = function (s) {
   let formatted = "";
   s = s.toLowerCase();
   for (let i = 0; i < s.length; i++) {
-    if (
-      s.charCodeAt(i) >= "a".charCodeAt(0) &&
-      s.charCodeAt(i) <= "z".charCodeAt(0)
-    ) {
-      formatted += s[i];
-    }
+      if (isAlphanumeric(s[i])) {
+          formatted += s[i];
+      }
   }
+
   let start = 0;
   let end = formatted.length - 1;
   while (start < formatted.length) {
-    if (formatted[start] !== formatted[end]) return false;
-    start++;
-    end--;
+      if (formatted[start] !== formatted[end]) return false;
+      start++;
+      end--;
   }
   return true;
 };
 
-console.log(isPalindrome("A man, a plan, a canal: Panama"));
+const isAlphanumeric = (char) => {
+  const charCode = char.charCodeAt(0)
+  return (charCode >= "a".charCodeAt(0) &&
+      charCode <= "z".charCodeAt(0)) || (charCode >= 48 && charCode <= 57)
+}
